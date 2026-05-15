53079ab56137c3f1bcd6c88fd062be934480ca2c
parom <E.Parominskiy@isida.by>
2026-05-15 10:47:54 +0300

    125. MySQL Container: Test User can be created

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
@@ -1,20 +1,33 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.*;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 public class UsersControllerWithTestcontainersTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
     @Container
     @ServiceConnection
@@ -36,6 +49,45 @@ public class UsersControllerWithTestcontainersTest {
     void testContainerIsRunning() {
         assertTrue(mySQLContainer.isCreated(), "MySql container has not been created");
         assertTrue(mySQLContainer.isRunning(), "MySql container is not running");
+    }
+
+    @Test
+    @DisplayName("User can be created")
+//    @Order(1)
+    void testCreateUser_whenValidDetailsProvided_returnsUserDetails() throws JSONException {
+        JSONObject userDetailsRequestJson = new JSONObject();
+        userDetailsRequestJson.put("firstName", "Sergey");
+        userDetailsRequestJson.put("lastName", "Kargopolov");
+        userDetailsRequestJson.put("email", "test@test.com");
+        userDetailsRequestJson.put("password","12345678");
+        userDetailsRequestJson.put("repeatPassword", "12345678");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> request = new HttpEntity<>(userDetailsRequestJson.toString(), headers);
+
+        // Act
+        ResponseEntity<UserRest> createdUserDetailsEntity = testRestTemplate.postForEntity("/users",
+                request,
+                UserRest.class);
+        UserRest createdUserDetails = createdUserDetailsEntity.getBody();
+
+        // Assert
+        Assertions.assertEquals(HttpStatus.OK, createdUserDetailsEntity.getStatusCode());
+        Assertions.assertEquals(userDetailsRequestJson.getString("firstName"),
+                createdUserDetails.getFirstName(),
+                "Returned user's first name seems to be incorrect");
+        Assertions.assertEquals(userDetailsRequestJson.getString("lastName"),
+                createdUserDetails.getLastName(),
+                "Returned user's last name seems to be incorrect");
+        Assertions.assertEquals(userDetailsRequestJson.getString("email"),
+                createdUserDetails.getEmail(),
+                "Returned user's email seems to be incorrect");
+        Assertions.assertFalse(createdUserDetails.getUserId().trim().isEmpty(),
+                "User id should not be empty");
+
     }
 
 
