cb064bd6b6b21c80896c11d53dd1c68d3ae0d38a
parom <E.Parominskiy@isida.by>
2026-05-15 09:52:13 +0300

    tune MySqlContainer and dynamically override property source

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
@@ -1,6 +1,8 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -11,7 +13,17 @@ public class UsersControllerWithTestcontainersTest {
 
     @Container
     // static allows up test container for all methods in this class!
-    private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:9.7");
+    private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:9.7")
+            .withDatabaseName("photo_app")
+            .withUsername("sergey")
+            .withPassword("sergey");
+
+    @DynamicPropertySource // allows dynamically override property source
+    private static void overrideProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
 
 
 }
