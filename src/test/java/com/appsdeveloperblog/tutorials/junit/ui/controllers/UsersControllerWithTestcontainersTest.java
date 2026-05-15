a31977574194581d47da020e67b2627658eb9092
parom <E.Parominskiy@isida.by>
2026-05-15 09:35:41 +0300

    prepare test container for start test

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestcontainersTest.java
@@ -1,12 +1,17 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 public class UsersControllerWithTestcontainersTest {
 
+    @Container
+    // static allows up test container for all methods in this class!
+    private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:9.7");
 
 
 }
